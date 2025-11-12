667e3e49a33e909054b229e8a87e3d46fc8fdce3
Blazheiko <40579911+Blazheiko@gmail.com>
2025-11-12 15:01:22 +0200

    Comment out audio call functionality in ChatArea.vue to streamline call handling through event-based architecture. Update UI to reflect changes in call initiation logic.

--- src/components/ChatArea.vue
+++ src/components/ChatArea.vue
@@ -127,20 +127,20 @@ const startVideoCall = async () => {
 }
 
 // Функция для начала аудиозвонка
-const startAudioCall = () => {
-    if (!selectedContact.value) {
-        console.error('No contact selected for audio call')
-        return
-    }
+// const startAudioCall = () => {
+//     if (!selectedContact.value) {
+//         console.error('No contact selected for audio call')
+//         return
+//     }
 
-    console.log('Starting audio call with:', selectedContact.value.id)
+//     console.log('Starting audio call with:', selectedContact.value.id)
 
-    // Эмитируем событие для начала WebRTC аудиозвонка
-    eventBus.emit('webrtc_start_call', {
-        targetUserId: selectedContact.value.id,
-        callType: 'audio',
-    })
-}
+//     // Эмитируем событие для начала WebRTC аудиозвонка
+//     eventBus.emit('webrtc_start_call', {
+//         targetUserId: selectedContact.value.id,
+//         callType: 'audio',
+//     })
+// }
 
 // WebRTC функции теперь обрабатываются в App.vue через useWebRTC composable
 
@@ -585,7 +585,7 @@ onUnmounted(() => {
                     />
                 </svg>
             </button>
-            <button
+            <!-- <button
                 class="voice-call-button"
                 title="Start voice call"
                 @click="startAudioCall"
@@ -602,7 +602,7 @@ onUnmounted(() => {
                         d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56-.35-.12-.74-.03-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z"
                     />
                 </svg>
-            </button>
+            </button> -->
             <input
                 v-model="newMessage"
                 type="text"
